4a55900eb204f923f4194f83b820e61c6fabbe3d
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-12-02 10:02:09 -0300

    Add player spaceship, movements and boundaries

--- main.py
+++ main.py
@@ -11,13 +11,40 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+#player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def player(x,y):
+  screen.blit(playerImg,(x,y))
+
 #Game Loop
 running = True
 while running:
+  screen.fill((0,0,0))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  screen.fill((0,255,155))
+    #if key is pressed check whether it is right or left
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        playerX_change = -0.1
+      if event.key == pygame.K_RIGHT:
+        playerX_change = 0.1
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        playerX_change = 0
+
+
+  playerX += playerX_change
+  if playerX <= 0:
+    playerX = 0
+  elif playerX >= 736:
+    playerX = 736
+    
+  player(playerX,playerY) 
   pygame.display.update()
 
