232462a57ba140f9ed9f7694898ebac4441e8394
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-12-02 10:37:33 -0300

    Add enemy spaceship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #initialize the pygame
 pygame.init()
@@ -17,8 +18,18 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+#enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0.3
+enemyY_change = 40
+
 def player(x,y):
   screen.blit(playerImg,(x,y))
+
+def enemy(x,y):
+  screen.blit(enemyImg,(x,y))
 
 #Game Loop
 running = True
@@ -31,20 +42,30 @@ while running:
     #if key is pressed check whether it is right or left
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT:
-        playerX_change = -0.1
+        playerX_change = -0.3
       if event.key == pygame.K_RIGHT:
-        playerX_change = 0.1
+        playerX_change = 0.3
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
         playerX_change = 0
 
-
+  #player movement
   playerX += playerX_change
   if playerX <= 0:
     playerX = 0
   elif playerX >= 736:
     playerX = 736
-    
-  player(playerX,playerY) 
+
+  #enemy movement
+  enemyX += enemyX_change
+  if enemyX <= 0:
+    enemyX_change = 0.3
+    enemyY += enemyY_change
+  elif enemyX >= 736:
+    enemyX_change = -0.3
+    enemyY += enemyY_change
+
+  player(playerX,playerY)
+  enemy(enemyX,enemyY)
   pygame.display.update()
 
